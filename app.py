70da51055f945cbf9baef94887a9c505a5086a8b
alihadian92 <118992296+alihadian92@users.noreply.github.com>
2024-10-03 11:56:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,20 @@
-import requests
 import streamlit as st
+from gtts import gTTS
+from io import BytesIO
 
-def text_to_speech(text):
-    api_key = "AIzaSyBaKDrDQ55nFvS4JSYWZEf0kTZUn9wMmWQ"
-    url = f"https://texttospeech.googleapis.com/v1/text:synthesize?key={api_key}"
-    headers = {'Content-Type': 'application/json'}
-    data = {
-        "input": {"text": text},
-        "voice": {"languageCode": "en-US", "ssmlGender": "NEUTRAL"},
-        "audioConfig": {"audioEncoding": "MP3"}
-    }
-    response = requests.post(url, json=data, headers=headers)
-    if response.status_code == 200:
-        audio_content = response.json()['audioContent']
-        with open("output.mp3", "wb") as f:
-            f.write(audio_content.encode('latin1'))
-        return "output.mp3"
-    else:
-        st.error("Error: " + str(response.status_code))
+st.title("Simple Text to Speech Converter")
 
-# Streamlit app interface
-st.title("Text-to-Speech Web App")
-text = st.text_area("Enter text for voice-over")
-if st.button("Convert"):
-    if text:
-        output = text_to_speech(text)
-        st.audio(output, format="audio/mp3")
+text_input = st.text_area("Enter text to convert to speech")
+
+language = st.selectbox("Select language", ["en", "fr", "ru", "hi", "es"])
+
+if st.button("Generate my speech"):
+    if text_input:
+        tts = gTTS(text_input, lang=language)
+        
+        audio_stream = BytesIO()
+        tts.write_to_fp(audio_stream)
+        
+        st.audio(audio_stream)
     else:
         st.warning("Please enter some text.")
